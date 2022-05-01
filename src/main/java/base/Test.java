b1284d351c284719c91002ea61b2ff4e4b081488
han.yu <han.yu@trip.com>
2022-05-01 16:44:40 +0800

    init

---
+++ src/main/java/base/Test.java
@@ -0,0 +1,21 @@
+package base;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int a = 10;
+        // 把a输出10遍
+        for (int i = 0; i < a; i++) {
+            System.out.println(i);
+        }
+        // 把a输出10遍，每次在后面加hello wjk
+        for (int i = 0; i < a; i++) {
+            System.out.println(i + "hello wjk");
+        }
+        //给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。请你将两个数相加，并以相同形式返回一个表示和的链表
+
+
+
+
+    }
+}
